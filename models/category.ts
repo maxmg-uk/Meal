df8810c9edd5fc8abb731a9dd26123d733de3602
Max Mason-Gransby <maximus66@hotmail.co.uk>
2025-01-17 10:20:08 +0000

    ts

--- models/category.ts
+++ models/category.ts
@@ -2,7 +2,8 @@ class Category {
   id: string;
   title: string;
   color: string;
-  constructor(id, title, color) {
+
+  constructor(id:string, title:string, color:string) {
     this.id = id;
     this.title = title;
     this.color = color;
